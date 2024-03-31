a87b86772a5dd94fb02a8dcdde2578998146b8f4
nikita <nikitachertkov@gmail.com>
2024-03-31 23:43:08 +0500

    TrainingController into ChildrenServiceApplication added

--- children-service/src/main/java/org/spring/childrenservice/ChildrenServiceApplication.java
+++ children-service/src/main/java/org/spring/childrenservice/ChildrenServiceApplication.java
@@ -2,12 +2,18 @@ package org.spring.childrenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ChildrenServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChildrenServiceApplication.class, args);
-	}
 
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
---
+++ children-service/src/main/java/org/spring/childrenservice/api/controllers/TrainingController.java
@@ -0,0 +1,25 @@
+package org.spring.childrenservice.api.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.spring.childrenservice.usecases.ChildrenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/children")
+public class TrainingController {
+
+    private final ChildrenService childrenService;
+    private final String trainingServiceUrl = "http://localhost:8765"; // URL микросервиса training
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/training")
+    public ResponseEntity<String> getTrainingList() {
+        ResponseEntity<String> response = restTemplate.getForEntity(trainingServiceUrl + "/training/all", String.class);
+        return ResponseEntity.ok(response.getBody());
+    }
+}
