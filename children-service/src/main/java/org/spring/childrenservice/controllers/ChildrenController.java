e83b87d2f9127fc1df86da394dadf5c803d4ea97
nikita <nikitachertkov@gmail.com>
2024-03-31 17:36:47 +0500

    ports corrected to 8765 instance made id added

--- children-service/src/main/java/org/spring/childrenservice/controllers/ChildrenController.java
+++ children-service/src/main/java/org/spring/childrenservice/controllers/ChildrenController.java
@@ -1,5 +1,6 @@
 package org.spring.childrenservice.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("children")
 public class ChildrenController {
+    @Value("${eureka.instance.instance-id}")
+    private String port;
 
     @GetMapping("hello")
     public String helloController() {
-        return "Hello from children";
+        return "Hello from children from port: " + port;
     }
 }
--- training-service/src/main/java/org/spring/trainingservice/controllers/TrainingController.java
+++ training-service/src/main/java/org/spring/trainingservice/controllers/TrainingController.java
@@ -1,5 +1,6 @@
 package org.spring.trainingservice.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("training")
 public class TrainingController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String port;
+
     @GetMapping("hello")
-    public String hello () {
-        return "hello from trainig!!!!!!!!!!!!!!!!!!";
+    public String helloController() {
+        return "Hello from TRAINING from port: " + port;
     }
 }
