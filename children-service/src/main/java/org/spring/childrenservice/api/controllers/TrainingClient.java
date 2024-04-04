a8e5ac9d0c3c2a731067127539dfef65f928a6f1
nikita <nikitachertkov@gmail.com>
2024-04-04 12:09:45 +0500

    ALL WORKS!!!

--- children-service/src/main/java/org/spring/childrenservice/api/controllers/TrainingClient.java
+++ children-service/src/main/java/org/spring/childrenservice/api/controllers/TrainingClient.java
@@ -1,12 +1,10 @@
 package org.spring.childrenservice.api.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.childrenservice.persistance.dto.AuthRequest;
 import org.spring.childrenservice.usecases.ChildrenService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -16,7 +14,47 @@ public class TrainingClient {
 
     private final ChildrenService childrenService;
     private final String trainingServiceUrl = "http://localhost:8765"; // URL микросервиса training
+    private final String authTokenUrl = "http://localhost:8765/auth/token";
     private final RestTemplate restTemplate;
+
+
+    @PostMapping("/token")
+    public String getToken(@RequestBody AuthRequest authRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<AuthRequest> requestEntity = new HttpEntity<>(authRequest, headers);
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(authTokenUrl, requestEntity, String.class);
+        String jwttoken = responseEntity.getBody();
+        return jwttoken;
+    }
+
+    @GetMapping("/trainigheader")
+    public ResponseEntity<String> getTrainingListWithHeader() {
+        AuthRequest authRequest = new AuthRequest("Vasya", "root");
+        String jwtToken = getToken(authRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken); // Добавляем JWT токен в заголовок Authorization
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(trainingServiceUrl + "/training/all", HttpMethod.GET, entity, String.class);
+        return response;
+    }
+
+    @GetMapping("/trainigheader/{id}")
+    public ResponseEntity<String> getTrainingByIdWithHeader(@PathVariable Long id) {
+        AuthRequest authRequest = new AuthRequest("Vasya", "root");
+        String jwtToken = getToken(authRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken); // Добавляем JWT токен в заголовок Authorization
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(trainingServiceUrl + "/training/" + id, HttpMethod.GET, entity, String.class);
+        return response;
+    }
 
     @GetMapping("/training")
     public ResponseEntity<String> getTrainingList() {
---
+++ javaguru-gateway/src/main/java/by/javaguru/JavaguruGatewayApplication.java
@@ -0,0 +1,15 @@
+package by.javaguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JavaguruGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguruGatewayApplication.class, args);
+	}
+
+}
