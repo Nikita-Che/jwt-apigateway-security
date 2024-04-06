afb2dfc91bea3ca5b963a144b710b113f176d29a
nikita <nikitachertkov@gmail.com>
2024-04-06 17:52:21 +0500

    logging service time fixed

--- logging-service/src/main/java/org/example/loggingservice/service/LoggingService.java
+++ logging-service/src/main/java/org/example/loggingservice/service/LoggingService.java
@@ -5,14 +5,24 @@ import org.example.loggingservice.model.LoggingMessage;
 import org.example.loggingservice.repository.MessageRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class LoggingService {
     private final MessageRepository messageRepository;
 
     public void save(String message) {
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = currentTime.format(formatter);
+
+        String messageToAdd = formattedTime + " - " + message;
+
         LoggingMessage loggingMessage = new LoggingMessage();
-        loggingMessage.setMessage(message);
+        loggingMessage.setMessage(messageToAdd);
         messageRepository.save(loggingMessage);
     }
 }
